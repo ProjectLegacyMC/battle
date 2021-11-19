7d4ecbf6b8ae8e4cf6db939c8572e34f242e5623
BuildTools <unconfigured@null.spigotmc.org>
2021-11-19 21:49:07 +0200

    commited some stuff idk

--- Battle/src/main/java/projectlegacy/battle/battleStartCommand.java
+++ Battle/src/main/java/projectlegacy/battle/battleStartCommand.java
@@ -7,7 +7,7 @@ import org.bukkit.command.CommandSender;
 public class battleStartCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        sender.sendMessage("r");
+        sender.sendMessage("hello world");
         return false;
     }
 }
